05131c9d366ebd90f3971125d112b8c4af0aea5f
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-04-12 22:26:02 +0300

    Explored values and variables.

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,19 @@
 let js = "amazing";
-
-if (js === "amazing") alert("Javascript is FUN");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Okyanus";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Okyanus";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
