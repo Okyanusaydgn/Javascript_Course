042b6ea1ef5206434e4bfb29afdf895e2bccbb75
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-04-26 19:04:36 +0300

    🚀Implement array basics: creation, access, mutation, and calculations.

--- starter/fundemenal-part-2/script.js
+++ starter/fundemenal-part-2/script.js
@@ -140,6 +140,8 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1991, "Okyanus")); // 2037 - 1991 = 46 yaşında, 19 yıl kaldı
 console.log(yearsUntilRetirement(1970, "Mike"));    // 2037 - 1970 = 67 yaşında, emekli olmuş
 */
+
+/*
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
 // Test 1
@@ -166,3 +168,47 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const firstName = "Okyanus";
+const okyanus = [firstName, "Aydogan", 2037 - 1999, "developer", friends];
+console.log(okyanus);
+console.log(okyanus.length);
+
+// Exercise
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
