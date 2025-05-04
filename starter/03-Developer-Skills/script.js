1c35c36f0abd6a50d70e8fffeafaff2536c67756
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-05-04 22:27:24 +0300

    add useful VS Code extensions and start "how to problem solve" section🚀.

---
+++ starter/03-Developer-Skills/script.js
@@ -0,0 +1,25 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// TODO: Bu fonksiyonu optimize et
+// FIXME: Burada hata var, düzeltilecek
+// BUG: Şu case yanlış çalışıyor
+// NOTE: Dikkat, bu kısım özel durum
+
+// PROBLEM
+// We work for a company building a smart home thermometer. Our most recent task in this: "Given an array of themperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures ?
+// - What's a sensor error ?  And what to do ?
+
+// 2) Breaking up into sub-problems
+//  - How to ignore errors ?
+//  - Find max value in temp array
+//  - Find min value in temp array
+//  - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {};
