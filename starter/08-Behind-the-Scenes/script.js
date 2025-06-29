3298cd9abd23381be3df1612aeac193df4b16bd2
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-06-29 21:56:07 +0300

    Learned destructuring and added practice examples.

--- starter/08-Behind-the-Scenes/script.js
+++ starter/08-Behind-the-Scenes/script.js
@@ -194,3 +194,49 @@ var addArrow = (a, b) => {
 };
 addArrow(2, 3);
 */
+
+// Object References in Practise
+
+const jessica1 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+function marryPerson(originalPerson, newLastName) {
+  originalPerson.lastName = newLastName;
+  return originalPerson;
+}
+
+const marriedJessica = marryPerson(jessica1, 'Davis');
+
+// const marriedJessica = jessica1;
+// marriedJessica.lastName = 'David';
+
+console.log('Before:', jessica1);
+console.log('After:', marriedJessica);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  familiy: ['Alice', 'Bob'],
+};
+
+// Shallow copy
+const jessicaCopy = { ...jessica };
+jessicaCopy.lastName = 'David';
+
+// jessicaCopy.familiy.push('Mary');
+// jessicaCopy.familiy.push('John');
+
+// console.log('Before:', jessica);
+// console.log('After:', jessicaCopy);
+
+// Deep copy/ clone
+const jessicaClone = structuredClone(jessica);
+jessicaClone.familiy.push('Mary');
+jessicaClone.familiy.push('John');
+
+console.log('Original:', jessica);
+console.log('Clone:', jessicaClone);
