226702a356eef66f6597b95302980e1e8cb2e17f
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-07-03 21:12:34 +0300

    Practice: || and && short-circuiting.

--- starter/09-Data-Structures-Operators/script.js
+++ starter/09-Data-Structures-Operators/script.js
@@ -71,6 +71,38 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+console.log('--------OR----------');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null); // ikisi de falsy değer o zaman sonuncusu döner.
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('--------AND----------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas'); // ikisi de truthy değer o zaman sonuncusu döner.
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/*
+//////////////////////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
 
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
@@ -104,7 +136,10 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
+*/
+
 /*
+
 //////////////////////////////////////////////////////
 // The Spread Operator (...)
 const arr = [7, 8, 9];
