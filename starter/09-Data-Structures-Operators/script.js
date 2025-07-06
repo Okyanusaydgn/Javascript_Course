ed8cd684382053f7742fa27f1abab44a416247af
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-07-06 14:29:18 +0300

    feat: add goal display, average odds, formatted odds, and scorers count.

--- starter/09-Data-Structures-Operators/script.js
+++ starter/09-Data-Structures-Operators/script.js
@@ -70,6 +70,9 @@ const restaurant = {
   },
 };
 
+/*
+////////////////////////////////////////////////
+// Looping Objects: Object Keys, Values and Entries
 // Property NAMES
 const properties = Object.keys(openingHours);
 console.log(properties);
@@ -93,6 +96,7 @@ const entries = Object.entries(openingHours);
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
+*/
 
 /*
 ////////////////////////////////////////////////////
@@ -206,6 +210,133 @@ const game = {
   },
 };
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+/*
+///////////////////////////////////////////////////////////
+// My Solution
+
+// 1.
+for (const [key, value] of game.scored.entries()) {
+  console.log(`Goal ${key + 1}: ${value}`);
+}
+
+// 2.
+let sum = 0;
+const oddsValues = Object.values(game.odds);
+for (const odd of oddsValues) {
+  sum += odd;
+}
+const average = sum / oddsValues.length;
+console.log(average);
+
+// 3.
+for (const [key, value] of Object.entries(game.odds)) {
+  const teamStr = key === 'x' ? 'draw' : `victory ${game[key]}`;
+
+  console.log(`Odd of ${teamStr}: ${value}`);
+}
+
+// 4.(BONUS)
+const scorers = {};
+
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+*/
+
+/*
+//////////////////////////////////////////////////////////////////////////////////////
+// Teacher solution
+
+// 1.
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) {
+  average += odd;
+}
+
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+*/
+
+/*
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+// My solution
+// Answer 1.1
+[players1, players2] = game.players;
+
+// Answer 1.2
+[gk, ...fieldPlayers] = game.players[0];
+
+// Answer 1.3
+const allPlayers = [...players1, ...players2];
+
+// Answer 1.4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// Answer 1.5
+const { team1, x: draw, team2 } = game.odds;
+
+// Answer 1.6
+printGoals = function (...players) {
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i]);
+  }
+
+  console.log(`Total goals: ${players.length}`);
+};
+
+// Answer 1.7
+game.odds.team1 < game.odds.team2 &&
+  console.log('Team 1 is more likely to win.');
+game.odds.team2 < game.odds.team1 &&
+  console.log('Team 2 is more likely to win.');
+*/
 /*
 //////////////////////////////////////////////////////////////////////////////////////
 // Teacher solution
@@ -243,42 +374,7 @@ printGoals(...game.scored);
 // 7.
 team1 < team2 && console.log('Team 1 is more likely to win.');
 team2 < team1 && console.log('Team 2 is more likely to win.');
-*/
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-
-// My solution
-// Answer 1.1
-[players1, players2] = game.players;
-
-// Answer 1.2
-[gk, ...fieldPlayers] = game.players[0];
-
-// Answer 1.3
-const allPlayers = [...players1, ...players2];
-
-// Answer 1.4
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-
-// Answer 1.5
-const { team1, x: draw, team2 } = game.odds;
-
-// Answer 1.6
-printGoals = function (...players) {
-  for (let i = 0; i < players.length; i++) {
-    console.log(players[i]);
-  }
-
-  console.log(`Total goals: ${players.length}`);
-};
-
-// Answer 1.7
-game.odds.team1 < game.odds.team2 &&
-  console.log('Team 1 is more likely to win.');
-game.odds.team2 < game.odds.team1 &&
-  console.log('Team 2 is more likely to win.');
 */
 
 ////////////////////////////////////////////////////////////////////////////////////////////
