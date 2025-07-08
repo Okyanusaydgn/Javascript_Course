0079d5ce07c805e3d1b5f8ba44fa9774517d5bd7
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-07-08 22:26:44 +0300

    Practice using JavaScript Map: creation, iteration, and conversion between Map and Object/Array.

--- starter/09-Data-Structures-Operators/script.js
+++ starter/09-Data-Structures-Operators/script.js
@@ -53,6 +53,51 @@ const restaurant = {
   },
 };
 
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 👌'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+// if (answer === 3) {
+//   console.log(question.get(true));
+// } else {
+//   console.log(question.get(false));
+// }
+
+/*
+////////////////////////////////////////////////////////////
+// Maps: Fundamentals
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 console.log(rest.set(2, 'Lisbon, Portugal'));
@@ -83,6 +128,7 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
+*/
 
 /*
 /////////////////////////////////////////////////////////////////////////////
