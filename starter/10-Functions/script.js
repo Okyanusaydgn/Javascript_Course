c8a6680afacc91834f17084b3a4a504f2180bd5e
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-07-21 09:25:57 +0300

    Reviewed and tested basic array methods.

--- starter/10-Functions/script.js
+++ starter/10-Functions/script.js
@@ -408,6 +408,8 @@ booker();
 console.dir(booker);
 
 // bir fonksiyonun tanımlandığı yere göre hangi değişkenlere ulaşabileceğini gösteren kurallar bütünü -- lexical scope
+
+// Bir fonksiyon, tanımlandığı scope’taki değişkenleri hafızada tutarak daha sonra da kullanabiliyorsa, buna closure denir.
 */
 
 // Closure Examples
@@ -451,4 +453,32 @@ const boardPassengers = function (n, wait) {
 };
 // const perGroup = 1000;
 boardPassengers(180, 3);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+
+// IIFE fonksiyonunun içinde header DOM elementine erişildi. Bu değişken, IIFE çalıştıktan sonra da addEventListener içinde tanımlanan fonksiyon tarafından erişilebildi çünkü bu fonksiyon, header’ın tanımlandığı lexical scope içinde yazılmıştı. Bu duruma closure denir. Yani bir fonksiyon, tanımlandığı yerdeki değişkenleri unutmaz, onlara erişmeye devam edebilir.
+
 */
