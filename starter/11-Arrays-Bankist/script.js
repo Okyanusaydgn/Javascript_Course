ede7bb29bf7331325cf9bd991f76929d0d377e45
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-07-27 17:26:11 +0300

    Add solutions for 7 tasks + bonus; implement sorting feature in Bankist app using sort() method.

--- starter/11-Arrays-Bankist/script.js
+++ starter/11-Arrays-Bankist/script.js
@@ -61,10 +61,13 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
   // .textContent = 0
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
         <div class="movements__row">
@@ -214,6 +217,13 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
@@ -591,6 +601,7 @@ console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 */
 
+/*
 ///////////////////////////
 // flat & flatMap method
 
@@ -619,3 +630,143 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance);
+*/
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+/*
+/////////////////////////////
+// MY SOLVING
+
+// 1 -
+const huskyWeight = breeds.find(dog => dog.breed === 'Husky').averageWeight;
+console.log(huskyWeight);
+
+// 2-
+const dogBothActivities = breeds.find(
+  dog => dog.activities.includes('running') && dog.activities.includes('fetch')
+).breed;
+
+console.log(dogBothActivities);
+
+// 3-
+const allActivities = breeds.flatMap(act => act.activities);
+console.log(allActivities);
+
+// 4-
+const uniqueActivities = [...new Set(allActivities)];
+console.log(uniqueActivities);
+
+// 5-
+const swimmingAdjacent = [
+  ...new Set(
+    breeds
+      .filter(swimLike => swimLike.activities.includes('swimming'))
+      .flatMap(act => act.activities)
+      .filter(activity => activity !== 'swimming')
+  ),
+];
+
+console.log(swimmingAdjacent);
+
+// 6-
+const dogAverage = breeds.every(weight => weight.averageWeight >= 10);
+console.log(dogAverage);
+
+// 7-
+const anyActive = breeds.some(act => act.activities.length >= 3);
+console.log(anyActive);
+
+// BONUS QUESTION
+
+const heavyDog = breeds
+  .filter(dog => dog.activities.includes('fetch'))
+  .map(avg => avg.averageWeight);
+
+console.log(Math.max(...heavyDog));
+*/
+
+//////////////////////////
+// SORTING ARRAYS
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0  A,B (keep order)
+// RETURN > 0  B,A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
