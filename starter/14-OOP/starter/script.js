0de3ba919eeefaec39c07cffb01b046daddd8f83
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-08-30 21:35:51 +0300

    Encapsulation and chainable methods practice with private/public/static fields and methods

--- starter/14-OOP/starter/script.js
+++ starter/14-OOP/starter/script.js
@@ -529,6 +529,7 @@ jay.introduce();
 jay.calcAge();
 */
 
+/*
 ///////////////////////////////////////////////
 // Another Class Example
 
@@ -573,3 +574,80 @@ acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 console.log(acc1);
+*/
+
+///////////////////////////////////////////////
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// STATIC version of these 4
+
+class Account {
+  locale = navigator.language;
+  bank = 'Bankist';
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    // this.movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  getMovements() {
+    return this.#movements;
+    // Not chainable
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  #approveLoan(val) {
+    // Fake method
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+    return this;
+  }
+
+  // static test() {
+  //   console.log('TEST');
+  // }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+// acc1.deposit(300);
+// acc1.withdraw(100);
+const movements = acc1
+  .deposit(300)
+  .withdraw(100)
+  .withdraw(50)
+  .requestLoan(25000)
+  .withdraw(4000)
+  .getMovements();
+console.log(acc1);
+// acc1.requestLoan(1000);
+// console.log(acc1.#movements);
+// acc1.#approveLoan(323);
+// Account.test();
+console.log(movements);
