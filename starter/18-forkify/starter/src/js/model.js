fca2128ff50c26833f5b63d087da8e5291702a34
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-10 18:36:13 +0300

    feat: add helpers and configuration files.

--- starter/18-forkify/starter/src/js/model.js
+++ starter/18-forkify/starter/src/js/model.js
@@ -1,17 +1,13 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e863d'
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} ${res.status} `);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -26,6 +22,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    // Temp error handling
+    console.error(`err ❌❌❌`);
   }
 };
