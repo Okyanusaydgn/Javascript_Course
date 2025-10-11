c79738c03f5101dccd4743be3f8908c192635e65
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-11 20:41:53 +0300

    feat(search): implement search results fetching & state (part 1).

--- starter/18-forkify/starter/src/js/config.js
+++ starter/18-forkify/starter/src/js/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
-export const TIMEOUT_SEC = 20;
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 40;
--- starter/18-forkify/starter/src/js/controller.js
+++ starter/18-forkify/starter/src/js/controller.js
@@ -1,21 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// NEW API URL (instead of the one shown in the video)
-// https://forkify-api.jonas.io
-
-///////////////////////////////////////
-//forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -30,8 +22,26 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
