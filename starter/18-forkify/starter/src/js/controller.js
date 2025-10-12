e7d7fec476f8299947bf835f065cdee91119636f
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-12 14:14:26 +0300

    feat(pagination): implement search results paging (part 1).

--- starter/18-forkify/starter/src/js/controller.js
+++ starter/18-forkify/starter/src/js/controller.js
@@ -40,7 +40,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearcResultsPage());
   } catch (err) {
     console.log(err);
   }
