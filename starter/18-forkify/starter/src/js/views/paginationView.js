d3a3b81637e0bf5929643e57228a91e6c7c9764f
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-14 09:19:17 +0300

    implement prev/next interactions (part 2)

---
+++ starter/18-forkify/starter/src/js/views/paginationView.js
@@ -0,0 +1,92 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    if (numPages <= 1) return '';
+
+    const prev = `
+    <button data-goto="${
+      curPage - 1
+    }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon"><use href="${icons}#icon-arrow-left"></use></svg>
+      <span>Page ${curPage - 1}</span>
+    </button>;
+    `;
+
+    const next = `
+    <button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon"><use href="${icons}#icon-arrow-right"></use></svg>
+    </button>`;
+
+    if (curPage === 1) return next;
+    if (curPage === numPages) return prev;
+    return next + prev;
+
+    // if (curPage === 1 && numPages > 1) {
+    //   return `
+    //       <button class="btn--inline pagination__btn--next">
+    //         <span>Page ${curPage + 1}</span>
+    //         <svg class="search__icon">
+    //           <use href="${icons}#icon-arrow-right"></use>
+    //         </svg>
+    //       </button>
+    //   `;
+    // }
+    // // Page 1, and there are NO other pages
+
+    // // Last page
+    // if (curPage === numPages && numPages > 1) {
+    //   return `
+    //       <button class="btn--inline pagination__btn--prev">
+    //         <svg class="search__icon">
+    //           <use href="${icons}#icon-arrow-left"></use>
+    //         </svg>
+    //         <span>Page ${curPage - 1}</span>
+    //       </button>
+    //   `;
+    // }
+    // // Other page
+    // if (curPage < numPages) {
+    //   return `
+    //       <button class="btn--inline pagination__btn--prev">
+    //         <svg class="search__icon">
+    //           <use href="${icons}#icon-arrow-left"></use>
+    //         </svg>
+    //         <span>Page ${curPage - 1}</span>
+    //       </button>
+
+    //        <button class="btn--inline pagination__btn--next">
+    //         <span>Page ${curPage + 1}</span>
+    //         <svg class="search__icon">
+    //           <use href="${icons}#icon-arrow-right"></use>
+    //         </svg>
+    //       </button>
+    //   `;
+    // }
+
+    // Page 1, and there are other pages
+    // return '';
+  }
+}
+
+export default new PaginationView();
