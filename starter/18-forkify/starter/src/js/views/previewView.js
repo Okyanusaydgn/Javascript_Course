0d2718eb22f6e2c4e239b2bbf498e1a78f041139
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-18 18:10:08 +0300

    feat(bookmarks): store bookmarks in localStorage.

--- starter/18-forkify/starter/src/js/views/previewView.js
+++ starter/18-forkify/starter/src/js/views/previewView.js
@@ -4,7 +4,7 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 class PreviewView extends View {
   _parentElement = '';
 
-  _generateMarkupPreview() {
+  _generateMarkup() {
     const id = window.location.hash.slice(1);
 
     return `
