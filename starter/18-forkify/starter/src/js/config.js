f1893feed73b87a8fa7e66dc0d6bf50d623f8dca
Okyanus Aydogan <okyanusaydgn@gmail.com>
2025-10-19 16:31:37 +0300

    feat(add-recipe): wire up upload form (part 2).

--- starter/18-forkify/starter/src/js/config.js
+++ starter/18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 40;
 export const RES_PER_PAGE = 10;
+export const KEY = '7a8b26d2-f8f4-4526-a8e0-fe85ee7d84f0';
+export const MODAL_CLOSE_SEC = 2.5;
